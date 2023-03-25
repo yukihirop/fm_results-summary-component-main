58adf6f5cedd7c6cd02167c4be9d9346286a175e
yukihirop <te108186@gmail.com>
2023-03-25 18:00:26 +0900

    feat: 🎸 initial commit

---
+++ styles/media-query.ts
@@ -0,0 +1,11 @@
+export const sm = `@media screen and (max-width: 640px)`;
+export const md = `@media screen and (max-width: 768px)`;
+export const lg = `@media screen and (max-width: 1024px)`;
+export const xl = `@media screen and (max-width: 1280px)`;
+export const xl2 = `@media screen and (max-width: 1536px)`;
+
+export const xsm = `@media screen and (min-width: 640px)`;
+export const xmd = `@media screen and (min-width: 768px)`;
+export const xlg = `@media screen and (min-width: 1024px)`;
+export const xxl = `@media screen and (min-width: 1280px)`;
+export const xxl2 = `@media screen and (min-width: 1536px)`;
