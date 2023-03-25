6ea26be6a61ce36a741328af1c0a1fb10714f1e2
yukihirop <te108186@gmail.com>
2023-03-25 18:08:52 +0900

    fix: 🐛 next config

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => (
+  <Html lang="ja">
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
